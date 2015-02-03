8fcfa06501097383bef4946ba2ba847db1e10691
Thomas Lee <thlee19190@gmail.com>
2015-02-03 05:40:51 +0000

    First Version - Pirate Bartender

--- pirate_bartender/pirate_bartender.py
+++ pirate_bartender/pirate_bartender.py
@@ -1,3 +1,5 @@
+import random
+
 #Dictonaries for pirate bartender project
 questions = {
   "strong":"Do ye like yer drinks strong?",
@@ -18,15 +20,26 @@ ingredients = {
 def picking_pref():
   """Ask the questions in question dictionary and replace the answers with boolean answers """
   quest_ans = {}
+  """Get preferences from the user and prompt them to answer questions"""
   for key in questions:
     print questions[key]
-    quest_ans[key] = raw_input("Please enter yes or y: ").lower()
-    return quest_ans
-    
+    temp = raw_input("Please enter yes or y: ").lower()
+    quest_ans[key] = temp == "y" or temp == "yes"
+  return quest_ans  
+
+def cons_drink(quest_ans):
+  drinks = []
+  for key2 in quest_ans:
+    if quest_ans[key2] == True:
+      drinks.append(random.choice(ingredients[key2]))
+    else:
+      continue
+  return drinks    
+  
 if __name__ == '__main__':
-   picking_pref()
-      
-      
+  answers = picking_pref()
+  answers2 = cons_drink(answers)
+  print answers2
       
       
       
